37c295ce8b59db461be411df18c8daa9bedc823a
Alex <188hardbass@gmail.com>
2024-02-10 20:37:07 +0200

    3

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({children}:Props) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        <SideNav />qwerty1
+        {/* <SideNav />qwerty1 */}
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
